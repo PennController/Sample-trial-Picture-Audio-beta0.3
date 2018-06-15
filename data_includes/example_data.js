364bd668237a11103f06840cfe3cd457c6b20d16
Jeremy <jeremyzehr@huntsman-ve506-0161.apn.wlan.upenn.edu>
2018-06-15 18:38:07 -0400

    Sample trial: Picture + Audio

---
+++ data_includes/example_data.js
@@ -0,0 +1,30 @@
+var shuffleSequence = seq("trial");
+PennController.ResetPrefix(null);
+
+PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
+
+var items = [
+    ["trial", "PennController", PennController(
+        image.defaults.settings.size(200, 200)
+        ,
+        // An image with 2 fish that swim in a round tank
+        newImage("target", "2fishRoundTank.png")
+        ,
+        // An image with 1 fish that swims in a square tank
+        newImage("competitor", "1fishSquareTank.png")
+        ,
+        newCanvas("patches", 450, 200)
+            .settings.add(  0, 0, getImage("competitor"))
+            .settings.add(250, 0, getImage("target"))
+            .print()
+        ,
+        // We add an audio element (audio file at the specified default URL)
+        // "The fish swim in a tank which is perfectly round"
+        newAudio("test sentence", "fishRound.ogg")
+            .play() // And we play it
+        ,
+        newSelector("fish")
+            .settings.add(getImage("competitor"), getImage("target"))
+            .wait()
+    )]
+];
